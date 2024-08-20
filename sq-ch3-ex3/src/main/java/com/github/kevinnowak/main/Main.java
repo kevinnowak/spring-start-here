065fd44751938e6637d4249821887accfcfa484d
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2024-08-20 23:01:48 +0200

    Update

---
+++ sq-ch3-ex3/src/main/java/com/github/kevinnowak/main/Main.java
@@ -0,0 +1,17 @@
+package com.github.kevinnowak.main;
+
+import com.github.kevinnowak.beans.Parrot;
+import com.github.kevinnowak.beans.Person;
+import com.github.kevinnowak.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+
+        System.out.printf("Person's name: %s\n", person.getName());
+        System.out.printf("Person's parrot: %s\n", person.getParrot());
+    }
+}
---
+++ sq-ch3-ex4/src/main/java/com/github/kevinnowak/beans/Person.java
@@ -0,0 +1,29 @@
+package com.github.kevinnowak.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name = "Elle";
+
+    @Autowired
+    private Parrot parrot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+}
