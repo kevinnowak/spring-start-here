dc7065952f9304c3342f2b60334ae9af8a152b7b
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-01-11 02:12:58 +0100

    Update

--- sq-ch4-ex1/src/test/java/AppTest.java
+++ sq-ch4-ex1/src/test/java/AppTest.java
@@ -14,14 +14,16 @@ public class AppTest {
             Verify that CommentService correctly delegates the responsibilities to the repository and proxy objects
             """)
     public void testCommentService() {
+	// Given
         var comment = mock(Comment.class);
         var commentRepository = mock(CommentRepository.class);
         var commentNotificationProxy = mock(CommentNotificationProxy.class);
-
         var commentService = new CommentService(commentRepository, commentNotificationProxy);
 
+	// When
         commentService.publishComment(comment);
 
+	// Then
         verify(commentRepository).storeComment(comment);
         verify(commentNotificationProxy).sendComment(comment);
     }
