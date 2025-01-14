f6ee9cf7117ba6e8324fbc290c409a438855946b
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-01-14 01:50:00 +0100

    Update

---
+++ sq-ch4-ex2/src/main/java/com/github/kevinnowak/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.github.kevinnowak.service;
+
+import com.github.kevinnowak.model.Comment;
+import com.github.kevinnowak.proxy.CommentNotificationProxy;
+import com.github.kevinnowak.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
---
+++ sq-ch4-ex2/src/test/java/AppTest.java
@@ -0,0 +1,31 @@
+import com.github.kevinnowak.model.Comment;
+import com.github.kevinnowak.proxy.CommentNotificationProxy;
+import com.github.kevinnowak.repository.CommentRepository;
+import com.github.kevinnowak.service.CommentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("""
+            Verify that CommentService correctly delegates the responsibilities to the repository and proxy objects
+            """)
+    public void testCommentService() {
+	// Given
+        var comment = mock(Comment.class);
+        var commentRepository = mock(CommentRepository.class);
+        var commentNotificationProxy = mock(CommentNotificationProxy.class);
+        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+
+	// When
+        commentService.publishComment(comment);
+
+	// Then
+        verify(commentRepository).storeComment(comment);
+        verify(commentNotificationProxy).sendComment(comment);
+    }
+}
