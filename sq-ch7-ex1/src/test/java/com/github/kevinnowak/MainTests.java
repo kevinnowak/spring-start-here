80520f5649552a2d5d58cd793cc009b6acc3f749
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-08-09 23:17:45 +0200

    chore: update

--- sq-ch7-ex1/src/test/java/com/github/kevinnowak/MainTests.java
+++ sq-ch7-ex1/src/test/java/com/github/kevinnowak/MainTests.java
@@ -1,13 +1,26 @@
 package com.github.kevinnowak;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MainTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    @DisplayName("Test that /home page can be successfully called.")
+    void testPageRequestAndContent() throws Exception {
+        mockMvc.perform(get("/home")).andExpect(status().isOk());
     }
 
 }
