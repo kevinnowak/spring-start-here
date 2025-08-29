5eaf931a743e79c892619f1876b4fec0e9afa9e4
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-08-30 00:44:17 +0200

    Update

---
+++ sq-ch9-ex3/src/main/java/com/github/kevinnowak/model/LoginProcessor.java
@@ -0,0 +1,51 @@
+package com.github.kevinnowak.model;
+
+import com.github.kevinnowak.services.LoggedUserManagementService;
+import com.github.kevinnowak.services.LoginCountService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+    private String username;
+    private String password;
+
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+        String username = this.getUsername();
+        String password = this.getPassword();
+        boolean loginResult = false;
+
+        if ("magdalena".equals(username) && "password".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+
+        return loginResult;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
