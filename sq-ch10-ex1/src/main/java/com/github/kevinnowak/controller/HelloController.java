c38b5496dcafe3c8eb6e64db66937c0f9317464f
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-08-31 01:00:05 +0200

    Update

--- sq-ch10-ex1/src/main/java/com/github/kevinnowak/controller/HelloController.java
+++ sq-ch10-ex1/src/main/java/com/github/kevinnowak/controller/HelloController.java
@@ -1,20 +1,19 @@
-package com.github.kevinnowak;
+package com.github.kevinnowak.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "Hello!";
     }
 
     @GetMapping("/ciao")
-    @ResponseBody
     public String ciao() {
         return "Ciao!";
     }
--- sq-ch10-ex1/src/test/java/com/github/kevinnowak/MainTest.java
+++ sq-ch10-ex1/src/test/java/com/github/kevinnowak/MainTest.java
@@ -1,4 +1,33 @@
 package com.github.kevinnowak;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MainTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello!"));
+    }
+
+    @Test
+    public void ciaoTest() throws Exception {
+        mockMvc.perform(get("/ciao"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Ciao!"));
+    }
 }
