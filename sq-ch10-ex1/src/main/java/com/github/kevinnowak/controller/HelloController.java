13c4108a5a8499061b20e42c725e1f0ec63fd049
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-09-01 23:21:35 +0200

    Update

--- sq-ch10-ex1/src/main/java/com/github/kevinnowak/controller/HelloController.java
+++ sq-ch10-ex1/src/main/java/com/github/kevinnowak/controller/HelloController.java
@@ -3,17 +3,18 @@ package com.github.kevinnowak.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello!";
     }
 
     @GetMapping("/ciao")
+    @ResponseBody
     public String ciao() {
         return "Ciao!";
     }
--- sq-ch10-ex2/src/test/java/com/github/kevinnowak/MainTest.java
+++ sq-ch10-ex2/src/test/java/com/github/kevinnowak/MainTest.java
@@ -1,4 +1,4 @@
-package kevinnowak;
+package com.github.kevinnowak;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
