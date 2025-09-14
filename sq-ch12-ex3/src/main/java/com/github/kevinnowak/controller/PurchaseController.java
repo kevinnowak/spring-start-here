f72acf434f43432b68edb674f82525f37850fb2e
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-09-15 00:28:20 +0200

    Update

---
+++ sq-ch12-ex3/src/main/java/com/github/kevinnowak/controller/PurchaseController.java
@@ -0,0 +1,28 @@
+package com.github.kevinnowak.controller;
+
+import com.github.kevinnowak.model.Purchase;
+import com.github.kevinnowak.repositories.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
